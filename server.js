eb0b673d20ac52e291ff18b2085f464d972bf14f
cpuspak <chkrhn123@gmail.com>
2022-09-04 20:31:35 +0530

    changes for deployment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static('./dist/ecomfrontend'))
+app.use(express.static('./dist/ExpenseTracker_UI'))
 
 app.get('/*', (req, res) => {
     res.sendFile('index.html', {root: 'dist/ExpenseTracker_UI'})
